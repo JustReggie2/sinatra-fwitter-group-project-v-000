8ee191c43ef755f233e8edd5b446485cf33c8495
Reginald T Carter iI <reginaldcarterii@gmail.com>
2019-01-14 01:20:21 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,45 +29,25 @@ class UsersController < ApplicationController
 		user = User.find_by(username: params[:username])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
-<<<<<<< HEAD
 			redirect "/tweets"
 		end
 			redirect '/login'
-=======
-			redirect 'tweets/tweets'
-		else
-			redirect 'users/login'
-		end
->>>>>>> d024950923d9d8e42e92cb3cd326304a1409d2d6
 	end
 
   get "/logout" do
     if Helpers.logged_in?(session)
       session.clear
-<<<<<<< HEAD
 
     end
 
     redirect "/login"
-=======
-    else
-      redirect '/'
-    end
-
-    redirect "/users/login"
->>>>>>> d024950923d9d8e42e92cb3cd326304a1409d2d6
 	end
 
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
     if @user
       erb :'users/show'
-<<<<<<< HEAD
 
-=======
-    else
-      redirect 'users/login'
->>>>>>> d024950923d9d8e42e92cb3cd326304a1409d2d6
     end
   end
 end
